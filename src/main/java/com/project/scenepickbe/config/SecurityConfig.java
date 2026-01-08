dc97d292d7576ec15a5dc7fd8534ecb90ead35c3
[CLOUD4] 한현 <gusgus1467@naver.com>
2026-01-08 23:36:06 +0900

    feat: 비밀번호 암호화 추가

--- src/main/java/com/project/scenepickbe/config/SecurityConfig.java
+++ src/main/java/com/project/scenepickbe/config/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.project.scenepickbe.config;
 
-import java.util.Collections;
-import java.util.List;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
-import lombok.RequiredArgsConstructor;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -41,5 +42,11 @@ public class SecurityConfig {
 			);
 
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		return new BCryptPasswordEncoder();
 	}
 }
