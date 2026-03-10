a8807af1ef8ad875f33a19ffe3ef5ccf7ab4e7ad
onhann <gusgus1467@naver.com>
2026-03-11 00:39:51 +0900

    ✨ [Feat] 카카오 OAuth 로그인 구현

--- src/main/java/com/project/scenepickbe/common/security/oauth/CustomerOauth2UserService.java
+++ src/main/java/com/project/scenepickbe/common/security/oauth/CustomerOauth2UserService.java
@@ -11,6 +11,8 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @RequiredArgsConstructor
 @Service
 public class CustomerOauth2UserService extends DefaultOAuth2UserService {
@@ -25,9 +27,34 @@ public class CustomerOauth2UserService extends DefaultOAuth2UserService {
 		String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
 		// 고유 식별값과 이메일 추출
-		String providerId = oAuth2USer.getAttribute("sub");
-		String email = oAuth2USer.getAttribute("email");
-		String name = oAuth2USer.getAttribute("name");
+		String providerId;
+		String email = null;
+		String name = null;
+
+		// 제공자에 따른 정보 추출 분기 처리
+		if ("kakao".equals(registrationId)) {
+			// 카카오는 고유 ID를 id로 줌
+			Object kakaoId = oAuth2USer.getAttribute("id");
+			providerId = String.valueOf(kakaoId);
+
+			// 이메일과 이름은 kakao_account와 profile 내부에 있음
+			Map<String, Object> kakaoAccount = (Map<String, Object>) oAuth2USer.getAttribute("kakao_account");
+
+			if (kakaoAccount != null) {
+				email = (String) kakaoAccount.get("email");
+				Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+				if (profile != null) {
+					name = (String) profile.get("nickname");
+				} else {
+					name = "KakaoUser";
+				}
+			}
+		} else {
+			// 구글 및 기본 설정
+			providerId = oAuth2USer.getAttribute("sub");
+			email = oAuth2USer.getAttribute("email");
+			name = oAuth2USer.getAttribute("name");
+		}
 
 		// DB 조회(providerId는 중복되지 않는 고유값)
 		UserVo userVo = userDao.selectUserByProvider(registrationId.toUpperCase(), providerId);
@@ -40,7 +67,7 @@ public class CustomerOauth2UserService extends DefaultOAuth2UserService {
 			String generatedUserId = registrationId.toLowerCase() + "_" + providerId.substring(0, 8);
 
 			// 회원가입 정보가 없는 경우 신규 저장
-			userVo = userVo.builder()
+			userVo = UserVo.builder()
 				.userId(generatedUserId)
 				.email(userEmail)
 				.username(name)
